edce4223734eb6442feae1598f86e080ada66e49
Freddy <freddy.oakes@me.com>
2015-02-21 12:50:27 -0500

    Added a function to calculate the times table called reCalculate(); Implemented the reLoad() function for the table. Forgot to commit most everything. Finished.

--- TimesTables/ViewController.swift
+++ TimesTables/ViewController.swift
@@ -1,25 +1,53 @@
-//
-//  ViewController.swift
-//  TimesTables
-//
-//  Created by Freddy Oakes on 2/21/15.
-//  Copyright (c) 2015 The Hofmeister Kink. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var theSliderValue = 0
+    var myResults = [0,0,0,0,0,0,0,0,0,0]
+    var myStringResults = ["","","","","","","","","",""]
+    var myNumbers = [1,2,3,4,5,6,7,8,9,10]
+
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var theTable: UITableView!
+    
+    @IBAction func updateSlider(sender: AnyObject) {
+        
+        reCalculate()
+        self.theTable.reloadData()
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return myNumbers.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        reCalculate()
+        cell.textLabel?.text = myStringResults[indexPath.row]
+        
+        return cell
+    }
+    
+    func reCalculate() {
+
+        for (var i = 0; i < 10; i++) {
+            theSliderValue = Int(slider.value)
+            myResults[i] = myNumbers[i] * theSliderValue
+            myStringResults[i] = "\(i) x \(theSliderValue) = \(myResults[i])"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
 
 }
 
